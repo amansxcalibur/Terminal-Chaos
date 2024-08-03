26722323379cfc6d08c54de936c383ce5f4d4c95
hridesh <hridesh699@gmail.com>
2024-08-03 15:03:35 +0530

    Dark realm 1 macdist

--- Arrakis-dex/Eolian Caves/Entrance3/01/01D/01/narrow path/path/0D/02/01/Chamber/chest1.py
+++ Arrakis-dex/Eolian Caves/Entrance3/01/01D/01/narrow path/path/0D/02/01/Chamber/chest1.py
@@ -5,6 +5,7 @@ if platform.system() == "Linux":
     p = subprocess.Popen([".linuxDist/dist/Chest1/Chest"])
     p.wait()
 if platform.system() == "Darwin":
-    print("Mac OS file not loaded yet")
+    p = subprocess.Popen([".macDist/dist/Chest1/Chest"])
+    p.wait()
 elif platform.system() == "Windows":
     print("What are yo doing on windows?")
