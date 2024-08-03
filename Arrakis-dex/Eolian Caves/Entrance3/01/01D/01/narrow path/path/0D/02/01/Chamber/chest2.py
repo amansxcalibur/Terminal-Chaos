bd3529868c72de040a5ceb8e8e9d1c4c5b0bf0d5
hridesh <hridesh699@gmail.com>
2024-08-03 15:08:03 +0530

    Dark realm 2 macdist

--- Arrakis-dex/Eolian Caves/Entrance3/01/01D/01/narrow path/path/0D/02/01/Chamber/chest2.py
+++ Arrakis-dex/Eolian Caves/Entrance3/01/01D/01/narrow path/path/0D/02/01/Chamber/chest2.py
@@ -5,11 +5,13 @@ import os
 if platform.system() == "Linux":
     p = subprocess.Popen([".linuxDist/dist/Chest/Chest"])
     p.wait()
-    try:
-        os.rename("DarkBookI.txt", "DarkBookII.txt")
-    except:
-        print("File not found! Please inform your mentor")
 if platform.system() == "Darwin":
-    print("Mac OS file not loaded yet")
+    p = subprocess.Popen([".macDist/dist/Chest/Chest"])
+    p.wait()
 elif platform.system() == "Windows":
-    print("What are yo doing on windows?")
+    print("What are you doing on windows?")
+
+try:
+    os.rename("DarkBookI.txt", "DarkBookII.txt")
+except:
+    pass
