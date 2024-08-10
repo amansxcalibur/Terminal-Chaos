3d3b5e9771ac135c203ce782f98d576f9948e4ed
amansxcalibur <notifyamanvs@gmail.com>
2024-08-10 09:02:16 +0530

    updated voidgate

--- Arrakis-dex/The Deep Desert E/9/4/Aridale/Voidgate Stronghold/Voidgate.sh
+++ Arrakis-dex/The Deep Desert E/9/4/Aridale/Voidgate Stronghold/Voidgate.sh
@@ -1,7 +1,9 @@
 echo "Enter code:"
 read -r encoded_input
-echo "Enter the activation password"
-read -r pass
-decoded_output=$(echo "$encoded_input" | xxd -r -p)
+#echo "Enter the activation password"
+#read -r pass
+#decoded_output=$(echo "$encoded_input" | xxd -r -p)
+decoded_output=$(echo "$encoded_input" | base64 -d)
 
-echo -e "\n$decoded_output/$pass"
+#echo -e "\n$decoded_output/$pass"
+echo -e "\n$decoded_output"
